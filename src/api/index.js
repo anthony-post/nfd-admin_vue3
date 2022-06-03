9c193ace74ddafa05e8743428bab731f8912d611
anthony-post <anthony-post@HOTMAIL.COM>
2022-06-03 22:36:13 +0300

    F-10 updated

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,6 @@ import axios from "axios";
 
 const basicToken = process.env.VUE_APP_BASE_ADMIN_TOKEN;
 
-// const loggedInUser = localStorage.getItem("user");
-// const tokenType = localStorage.getItem("token_type");;
-// const authType = tokenType[1].toUpperCase() + tokenType.slice(2, 7);
-const authType = "Bearer";
-const accessToken = localStorage.getItem("access_token");
-
 const loginConfig = {
   baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
@@ -23,8 +17,17 @@ const instanceConfig = {
   headers: {
     "X-Api-Factory-Application-Id": process.env.VUE_APP_APPLICATION_ID,
     "Content-Type": "application/json",
-    Authorization: `${authType} ${accessToken}`.replace(/['"]+/g, ""),
   },
 };
 
-export const entityAPIInstance = axios.create(instanceConfig);
+const APIInstance = axios.create(instanceConfig);
+
+APIInstance.interceptors.request.use(config => {
+  const tokenType = localStorage.getItem("token_type");
+  const authType = tokenType[1].toUpperCase() + tokenType.slice(2, 7);
+  const accessToken = localStorage.getItem("access_token");
+  config.headers.authorization = `${authType} ${accessToken}`.replace(/['"]+/g, "");
+  return config;
+});
+
+export const entityAPIInstance = APIInstance;
